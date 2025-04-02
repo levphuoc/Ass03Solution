ee54fa94c19fb1f6e84320daa43685340bf2a034
Duc1810 <nguyenbaminhduc2019@gmail.com>
2025-04-02 11:02:04 +0700

    update

--- BLL/Hubs/OrderHub.cs
+++ BLL/Hubs/OrderHub.cs
@@ -15,41 +15,27 @@ namespace BLL.Hubs
             _orderService = orderService;
         }
 
-        // Gửi thông báo cập nhật đơn hàng
        
-
-        // Notify clients when an order is created
         public async Task NotifyOrderCreation(int orderId)
         {
-            try
-            {
+           
+            
                 var order = await _orderService.GetOrderByIdAsync(orderId);
                 if (order != null)
                 {
-                    await Clients.All.SendAsync("OrderCreated", orderId); // Gửi thông báo với orderId
+                    await Clients.All.SendAsync("OrderCreated", orderId); 
                 }
                 else
                 {
                     Console.WriteLine($"Order not found with ID: {orderId}");
                 }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error while notifying order creation: {ex.Message}");
-            }
+           
         }
         public async Task NotifySpecificOrderUpdate(int orderId)
         {
-            try
-            {
-                
-                    // Gửi thông báo đến client với orderId cụ thể
+           
                     await Clients.All.SendAsync("SpecificOrderUpdated", orderId);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error while notifying specific order update: {ex.Message}");
-            }
+           
         }
     }
 }
